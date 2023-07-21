b1096f2aecf1b19999dcb0eaf0cb9ebfc42ab3b2
Pappu sanodiya <81160267+pappusanodiya@users.noreply.github.com>
2023-07-21 07:12:19 +0530

    Create Facecrop.py
    Face crop code

---
+++ Facecrop.py
@@ -0,0 +1,39 @@
+import cv2
+
+# Load the Haar Cascade classifier for face detection
+face_cascade = cv2.CascadeClassifier('haarcascase.xml')
+
+# Open the webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read the frame from the webcam
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # Process each detected face
+    for (x, y, w, h) in faces:
+        # Crop the detected face from the frame
+        face = frame[y:y+h, x:x+w]
+        
+        # Display the cropped face in a separate window
+        cv2.imshow('Detected Face', face)
+
+        # Draw a rectangle around the detected face in the original frame
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the frame with detected faces
+    cv2.imshow('Face Detection', frame)
+
+    # Exit the loop if the 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and close all windows
+cap.release()
+cv2.destroyAllWindows()
